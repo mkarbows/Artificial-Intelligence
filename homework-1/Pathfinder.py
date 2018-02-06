c28516675313404651b7e5025dcd6cccae8f9735
Megan Karbowski <mck064@gmail.com>
2018-02-05 20:30:15 -0800

    working on the pathfinder solve algorithm

--- homework-1/Pathfinder.py
+++ homework-1/Pathfinder.py
@@ -12,10 +12,19 @@ from SearchTreeNode import SearchTreeNode
 import unittest
 
 class Pathfinder:
-    
+
     @staticmethod
     def solve(problem):
         # TODO: Implement A* graph search!
+        # priority que :
+        # start at root node -> get f(n) -> push to queue
+         # pop off root -> add to resultpath -> push transitions & cooresponding costs to queue
+         # pop off node w min cost -> add to resultpath
+         # expand node to get transitions -> push transitions to queue
+         # continue until goal reached
+         resultpath = []
+         # are we allowed to import heapq library? to implement the priority queue
+
         return []
 
 class PathfinderTests(unittest.TestCase):
@@ -26,7 +35,7 @@ class PathfinderTests(unittest.TestCase):
         solnTest = problem.solnTest(soln)
         self.assertTrue(solnTest[1])
         self.assertEqual(solnTest[0], 4)
-    
+
     def test_maze2(self):
         maze = ["XXXXX", "XG..X", "XX..X", "X*..X", "XXXXX"]
         problem = MazeProblem(maze)
@@ -34,7 +43,7 @@ class PathfinderTests(unittest.TestCase):
         solnTest = problem.solnTest(soln)
         self.assertTrue(solnTest[1])
         self.assertEqual(solnTest[0], 4)
-        
+
     def test_maze3(self):
         maze = ["XXXXX", "X..GX", "X.MMX", "X*..X", "XXXXX"]
         problem = MazeProblem(maze)
@@ -42,13 +51,13 @@ class PathfinderTests(unittest.TestCase):
         solnTest = problem.solnTest(soln)
         self.assertTrue(solnTest[1])
         self.assertEqual(solnTest[0], 4)
-        
+
     def test_maze4(self):
         maze = ["XXXXXX", "X....X", "X*.XXX", "X..XGX", "XXXXXX"]
         problem = MazeProblem(maze)
         soln = Pathfinder.solve(problem)
         self.assertFalse(soln)
-        
+
 
 if __name__ == '__main__':
     unittest.main()
